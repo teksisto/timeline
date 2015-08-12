248a4d2bf6f365a3c80bd6d06dad0ae56d9196b5
teksisto <teksisto@gmail.com>
2015-08-12 18:43:37 +0300

    add: в модели Source связь с Person стала именованной -- authors
    Для этого пришлось прописать все данные для связи, хотя мне казалось,
    что можно просто написать:
    
      has_and_belongs_to_many :people, as: 'authors'

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,6 +1,9 @@
 class Source < ActiveRecord::Base
 
-  has_and_belongs_to_many :people
+  has_and_belongs_to_many :authors,
+                          class_name: Person,
+                          join_table: 'people_sources',
+                          association_foreign_key: 'person_id'
   
   has_many :events
   belongs_to :category
