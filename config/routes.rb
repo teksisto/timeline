7af91a4a527f5d7bc94b77059e66a1494fd4cd8a
teksisto <teksisto@gmail.com>
2015-08-12 17:35:12 +0300

    add: скаффолд для Person

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :people
   resources :resources do
     collection do
       get :fget
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150801150202) do
+ActiveRecord::Schema.define(version: 20150812142733) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 20150801150202) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "quotes", force: :cascade do |t|
