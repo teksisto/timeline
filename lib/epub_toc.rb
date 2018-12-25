9d8e7efb697cfa0280314350f05e7e991699ccdd
Alexander Antonov <teksisto@gmail.com>
2018-12-25 17:42:40 +0300

    add: extract anchor from epub

--- lib/epub_toc.rb
+++ lib/epub_toc.rb
@@ -13,6 +13,7 @@ class EpubToc
 
   def extract
     toc = nil
+    toc_prefix = ''
     Tempfile.create(['toc', '.epub']) do |tempfile|
       tempfile.binmode
       tempfile.write(@source.file.download)
@@ -20,6 +21,7 @@ class EpubToc
       Zip::File.open(tempfile.path) do |zip_file|
         entry = zip_file.glob('**/*.ncx').first
         if entry
+          toc_prefix = File.dirname(entry.name)
           toc = entry.get_input_stream.read
         else
           Rails.logger.error 'No ncx file.'
@@ -27,23 +29,49 @@ class EpubToc
       end
     end
     if toc.present?
-      init_extraction(toc)
+      init_extraction(toc, toc_prefix)
     end
   end
 
-  def init_extraction(toc)
+  def init_extraction(toc, toc_prefix)
     doc = REXML::Document.new(toc)
     doc.elements.each('*/navMap') do |map|
-      recursive(@source, map)
+      recursive(@source, map, toc_prefix)
     end
   end
 
-  def recursive(parent, doc)
+  def recursive(parent, doc, toc_prefix)
     doc.elements.each('navPoint') do |point|
-      label_node = point.elements.to_a('navLabel/text').first
-      label = label_node.text.to_s
-      child = Source.create(label: label, parent: parent)
-      recursive(child, point)
+      label  = extract_label(point)
+      anchor = extract_anchor(point, toc_prefix)
+
+      child = Source.create(label: label, parent: parent, anchor: anchor)
+
+      recursive(child, point, toc_prefix)
+    end
+  end
+
+  def extract_label(point)
+    label_node = point.elements.to_a('navLabel/text').first
+    label_node.text.to_s
+  end
+
+  def extract_anchor(point, toc_prefix)
+    anchor_node = point.elements.to_a('content').first
+    if sanitize_anchor(anchor_node['src'])
+      anchor = File.join(toc_prefix, sanitize_anchor(anchor_node['src']))
+    else
+      anchor = nil
+    end
+    anchor
+  end
+
+  def sanitize_anchor(raw_anchor)
+    array = raw_anchor.split('#')
+    if array.size > 1
+      return nil
+    else
+      return raw_anchor
     end
   end
 
